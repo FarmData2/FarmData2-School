2bfcdc35502c6f90595e218746f91d8239a20774
AnnaleeBr <brantan@dickinson.edu>
2025-09-16 10:39:30 -0400

    Adds heading and 2 input types, along with styles for both of them

--- modules/farm_fd2_school/src/entrypoints/HTML_CSS/App.vue
+++ modules/farm_fd2_school/src/entrypoints/HTML_CSS/App.vue
@@ -3,7 +3,36 @@
     id="HTML-CSS"
     data-cy="HTML-CSS"
   >
-    <p>Add HTML for Harvest form here.</p>
+    <h1>Harvest</h1>
+
+    <label
+      for="date"
+      class="input-rectangle"
+      >Date:</label
+    >
+    <input
+      type="date"
+      id="date"
+      value="2019-06-15"
+    />
+
+    <br />
+
+    <label
+      for="crop"
+      class="input-rectangle"
+      >Crop:</label
+    >
+    <select
+      name="crop"
+      id="crop"
+    >
+      <option>ARUGULA</option>
+      <option>ASPARAGUS</option>
+      <option>BEAN</option>
+      <option selected>RADISH</option>
+    </select>
+    <p>Testing commit</p>
   </div>
 </template>
 
@@ -14,4 +43,11 @@
 @import url('@css/fd2-mobile.css');
 
 /* Add CSS for harvest form here */
+h1 {
+  text-align: center;
+}
+
+.input-rectangle {
+  padding: 10px;
+}
 </style>
