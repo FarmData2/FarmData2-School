9533b4edd3cd8f2e343c84f7ef18146bb39e19b8
Cecee <kornreic@dickinson.edu>
2025-09-16 11:52:30 -0400

    Changes to table element

--- modules/farm_fd2_school/src/entrypoints/HTML_CSS/App.vue
+++ modules/farm_fd2_school/src/entrypoints/HTML_CSS/App.vue
@@ -27,6 +27,39 @@
       <option value="bean">BEAN</option>
     </select>
   </p>
+
+  <table>
+    <tr>
+      <th></th>
+      <th>Location</th>
+      <th>Bed</th>
+      <th>Planted Date</th>
+    </tr>
+    <tr>
+      <td></td>
+      <td>D</td>
+      <td></td>
+      <td>04/02/2019</td>
+    </tr>
+    <tr>
+      <td></td>
+      <td>GHANA</td>
+      <td>GHANA-2</td>
+      <td>02/04/2019</td>
+    </tr>
+    <tr>
+      <td></td>
+      <td>GHANA</td>
+      <td>GHANA-4</td>
+      <td>02/04/2019</td>
+    </tr>
+    <tr>
+      <td></td>
+      <td>E</td>
+      <td></td>
+      <td>06/05/2019</td>
+    </tr>
+  </table>
 </template>
 
 <script></script>
@@ -48,8 +81,26 @@ input {
 
 p,
 label {
-  font-family: sans-serif;
-  font-size: 1rem;
   padding-right: 10px;
+}
+
+p,
+select {
+  padding: 2px 5px;
+}
+
+p,
+table,
+tr,
+th {
+  border: 2px solid black;
+}
+
+p,
+table,
+tr {
+  border-bottom: 2px solid grey;
+  border-right: 4px solid black;
+  border-left: 4px solid black;
 }
 </style>
