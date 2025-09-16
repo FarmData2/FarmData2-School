4aa300fcc15b300b600dcba3215721a5794f81e5
robertli149 <robertli@dickinson.edu>
2025-09-16 17:54:13 -0400

    Added Location Selector

--- modules/farm_fd2_school/src/entrypoints/HTML_CSS/App.vue
+++ modules/farm_fd2_school/src/entrypoints/HTML_CSS/App.vue
@@ -33,6 +33,77 @@
         RADISH
       </option>
     </select>
+
+    <table class="table table-responsive">
+      <thead>
+        <tr>
+          <th></th>
+          <th>Location</th>
+          <th>Bed</th>
+          <th>Planted Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <form>
+          <tr>
+            <td>
+              <div class="radio">
+                <label
+                  ><input
+                    type="radio"
+                    name="line1"
+                /></label>
+              </div>
+            </td>
+            <td>D</td>
+            <td></td>
+            <td>04/02/2019</td>
+          </tr>
+          <tr>
+            <td>
+              <div class="radio">
+                <label
+                  ><input
+                    type="radio"
+                    name="line2"
+                /></label>
+              </div>
+            </td>
+            <td>GHANA</td>
+            <td>GHANA-2</td>
+            <td>04/02/2019</td>
+          </tr>
+          <tr>
+            <td>
+              <div class="radio">
+                <label
+                  ><input
+                    type="radio"
+                    name="line3"
+                /></label>
+              </div>
+            </td>
+            <td>GHANA</td>
+            <td>GHANA-4</td>
+            <td>04/02/2019</td>
+          </tr>
+          <tr>
+            <td>
+              <div class="radio">
+                <label
+                  ><input
+                    type="radio"
+                    name="line4"
+                /></label>
+              </div>
+            </td>
+            <td>E</td>
+            <td></td>
+            <td>06/05/2019</td>
+          </tr>
+        </form>
+      </tbody>
+    </table>
   </body>
 </template>
 
