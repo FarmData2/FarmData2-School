c1ac9327d5cbf452f7eccdb3982d0a85e602c391
jworobel <worobelj@dickinson.edu>
2025-09-17 11:29:03 -0400

    temporary table

--- modules/farm_fd2_school/src/entrypoints/HTML_CSS/App.vue
+++ modules/farm_fd2_school/src/entrypoints/HTML_CSS/App.vue
@@ -20,6 +20,42 @@
     </option>
     <option value="squash">Squash</option>
   </select>
+  <table>
+    <thead>
+      <tr>
+        <th></th>
+        <th>Column A</th>
+        <th>Column B</th>
+        <th>Column C</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><input type="checkbox" /></td>
+        <td>Data A1</td>
+        <td>Data B1</td>
+        <td>Data C1</td>
+      </tr>
+      <tr>
+        <td><input type="checkbox" /></td>
+        <td>Data A2</td>
+        <td>Data B2</td>
+        <td>Data C2</td>
+      </tr>
+      <tr>
+        <td><input type="checkbox" /></td>
+        <td>Data A3</td>
+        <td>Data B3</td>
+        <td>Data C3</td>
+      </tr>
+      <tr>
+        <td><input type="checkbox" /></td>
+        <td>Data A4</td>
+        <td>Data B4</td>
+        <td>Data C4</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script></script>
@@ -28,5 +64,31 @@
 /* import some styling that applies to all FD2 entry points */
 @import url('@css/fd2-mobile.css');
 
-/* Add CSS for harvest form here */
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+thread tr {
+  background-color: white;
+}
+
+tbody tr {
+  background-color: gray;
+}
+
+th,
+td {
+  padding: 10px;
+  text-align: left;
+}
+
+tr {
+  border-bottom: 1px solid #ccc;
+}
+
+th,
+td {
+  border: none;
+}
 </style>
