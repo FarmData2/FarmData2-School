49a47c800926d36bf1a1e134ba2470db4f1a3d3f
jworobel <worobelj@dickinson.edu>
2025-09-17 11:11:10 -0400

    added select feature

--- modules/farm_fd2_school/src/entrypoints/HTML_CSS/App.vue
+++ modules/farm_fd2_school/src/entrypoints/HTML_CSS/App.vue
@@ -1,21 +1,32 @@
-<!DOCTYPE html>
-<html lang="en-US">
-  <head>
-    <title>Harvest</title>
-  </head>
-
+<template>
+  <h1>Harvest</h1>
   <label for="date-input">Date:</label>
   <input
     type="date"
     value="2025-01-02"
   />
+  <br />
 
-  <script></script>
+  <select
+    name="crops"
+    id="crop-select"
+  >
+    <option value="broccoli">Broccoli</option>
+    <option
+      value="radish"
+      selected
+    >
+      Radish
+    </option>
+    <option value="squash">Squash</option>
+  </select>
+</template>
 
-  <style>
-    /* import some styling that applies to all FD2 entry points */
-    @import url('@css/fd2-mobile.css');
+<script></script>
 
-    /* Add CSS for harvest form here */
-  </style>
-</html>
+<style>
+/* import some styling that applies to all FD2 entry points */
+@import url('@css/fd2-mobile.css');
+
+/* Add CSS for harvest form here */
+</style>
