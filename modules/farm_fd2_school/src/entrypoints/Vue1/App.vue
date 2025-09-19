32eb3268d0c8951bf5cd2ffd86bca4f84c897c1f
jworobel <worobelj@dickinson.edu>
2025-09-19 13:22:04 -0400

    plant table now pulls from dynamic plant object list

--- modules/farm_fd2_school/src/entrypoints/Vue1/App.vue
+++ modules/farm_fd2_school/src/entrypoints/Vue1/App.vue
@@ -38,57 +38,19 @@
         <th>Bed</th>
         <th>Planted Date</th>
       </tr>
-      <tr>
+      <tr
+        v-for="(plant, index) in plants"
+        v-bind:key="index"
+      >
         <td>
           <input
             type="radio"
             name="harvest-plant"
-            id="harvest-plant1"
-            value="1"
           />
         </td>
-        <td>D</td>
-        <td></td>
-        <td>04/02/2019</td>
-      </tr>
-      <tr>
-        <td>
-          <input
-            type="radio"
-            name="harvest-plant"
-            id="harvest-plant2"
-            value="2"
-          />
-        </td>
-        <td>GHANA</td>
-        <td>GHANA-2</td>
-        <td>02/04/2019</td>
-      </tr>
-      <tr>
-        <td>
-          <input
-            type="radio"
-            name="harvest-plant"
-            id="harvest-plant3"
-            value="3"
-          />
-        </td>
-        <td>GHANA</td>
-        <td>GHANA-4</td>
-        <td>02/04/2019</td>
-      </tr>
-      <tr>
-        <td>
-          <input
-            type="radio"
-            name="harvest-plant"
-            id="harvest-plant4"
-            value="4"
-          />
-        </td>
-        <td>E</td>
-        <td></td>
-        <td>06/05/2019</td>
+        <td>{{ plant.location }}</td>
+        <td>{{ plant.bed }}</td>
+        <td>{{ plant.date }}</td>
       </tr>
     </table>
 
@@ -149,6 +111,12 @@ export default {
         { id: 2, label: 'ASPARAGUS' },
         { id: 3, label: 'BEAN' },
         { id: 4, label: 'RADISH' },
+      ],
+      plants: [
+        { id: 1, date: 'DATE', location: 'D', bed: '' },
+        { id: 2, date: 'DATE', location: 'GHANA', bed: 'GHANA-2' },
+        { id: 3, date: 'DATE', location: 'GHANA', bed: 'GHANA-4' },
+        { id: 4, date: 'DATE', location: 'GHANA', bed: '' },
       ],
     };
   },
