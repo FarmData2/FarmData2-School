c8b9f9e30e23d78bfeb8b8b65a14ac3115b4a50a
braughtg <braught@dickinson.edu>
2025-10-09 17:50:07 -0400

    wip

---
+++ library/farmosUtil/farmosUtil.harvest.js
@@ -0,0 +1,97 @@
+/*
+ * Utility functions for getting and working with harvest data.
+ */
+
+import dayjs from 'dayjs';
+import { getFarmOSInstance } from './farmosUtil.core.js';
+import { getCropIdToTermMap } from './farmosUtil.crops.js';
+import {
+  getPlantingLocationObjects,
+  getLogCategoryObjects,
+  getQuantityObjects,
+} from './farmosUtil.utilities.js';
+
+/**
+ * Create a new harvest log.
+ *
+ * @param {string} harvestDate the date of the harvest.
+ * @param {string} locationName the name of the location where the harvest occurred.
+ * @param {Array<string>} bedNames the names of the bed(s) where the harvest occurred.
+ * @param {Object} plantAsset the plant asset affected by the harvest.
+ * @param {Object} quantity the quantity associated with the harvest.
+ * @returns {Object} the new harvest log.
+ * @throws {Error} if unable to create the harvest log.
+ *
+ * @category Harvest
+ */
+export async function createHarvestLog(
+  harvestDate,
+  locationName,
+  bedNames,
+  plantAsset,
+  quantity
+) {
+  const locationsArray = await getPlantingLocationObjects([
+    locationName,
+    ...bedNames,
+  ]);
+
+  const quantitiesArray = getQuantityObjects([quantity]);
+
+  let logName = dayjs(harvestDate).format('YYYY-MM-DD');
+  logName += '_h_';
+  const cropIdToNameMap = await getCropIdToTermMap();
+  logName += plantAsset.relationships.plant_type
+    .map((crop) => cropIdToNameMap.get(crop.id).attributes.name)
+    .join('_');
+
+  console.log(logName);
+}
+
+/*
+  // Helper functions from the utilities module.
+  const locationsArray = await getPlantingLocationObjects([
+    locationName,
+    ...bedNames,
+  ]);
+  const logCategoriesArray = await getLogCategoryObjects(logCategories);
+  const quantitiesArray = getQuantityObjects(quantities);
+
+  const cropIdToNameMap = await getCropIdToTermMap();
+  let logName = dayjs(seedingDate).format('YYYY-MM-DD');
+  if (logCategories.includes('seeding_tray')) {
+    logName += '_ts_';
+  } else if (logCategories.includes('seeding_direct')) {
+    logName += '_ds_';
+  } else if (logCategories.includes('seeding_cover_crop')) {
+    logName += '_cs_';
+  }
+
+  logName += plantAsset.relationships.plant_type
+    .map((crop) => cropIdToNameMap.get(crop.id).attributes.name)
+    .join('_');
+
+  const seedingLogData = {
+    type: 'log--seeding',
+    attributes: {
+      name: logName,
+      timestamp: dayjs(seedingDate).format(),
+      status: 'done',
+      is_movement: true,
+      purchase_date: dayjs(seedingDate).format(),
+    },
+    relationships: {
+      location: locationsArray,
+      asset: [{ type: 'asset--plant', id: plantAsset.id }],
+      category: logCategoriesArray,
+      quantity: quantitiesArray,
+    },
+  };
+
+  const farm = await getFarmOSInstance();
+  const seedingLog = farm.log.create(seedingLogData);
+  await farm.log.send(seedingLog);
+
+  return seedingLog;
+}
+*/
---
+++ library/farmosUtil/farmosUtil.harvest.unit.cy.js
@@ -0,0 +1,82 @@
+import * as farmosUtil from './farmosUtil';
+
+describe('Test the harvest log functions', () => {
+  let fieldMap = null;
+  let bedMap = null;
+
+  beforeEach(() => {
+    cy.restoreLocalStorage();
+    cy.restoreSessionStorage();
+
+    cy.wrap(farmosUtil.getFieldNameToAssetMap()).then((map) => {
+      fieldMap = map;
+    });
+
+    cy.wrap(farmosUtil.getBedNameToAssetMap()).then((map) => {
+      bedMap = map;
+    });
+  });
+
+  afterEach(() => {
+    cy.saveLocalStorage();
+    cy.saveSessionStorage();
+  });
+
+  it('Create a harvest log', () => {
+    cy.wrap(
+      farmosUtil.createPlantAsset('1999-01-02', 'ARUGULA', 'testComment')
+    ).as('plantAsset');
+
+    cy.wrap(
+      farmosUtil.createStandardQuantity('weight', 5, 'harvest', 'POUND')
+    ).as('quantity');
+
+    cy.getAll(['@plantAsset', '@quantity']).then(([plantAsset, quantity]) => {
+      cy.wrap(
+        farmosUtil.createHarvestLog(
+          '01/02/1999',
+          'ALF',
+          [],
+          plantAsset,
+          quantity
+        )
+      ).as('harvestLog');
+    });
+
+    cy.getAll(['@plantAsset', '@quantity', '@harvestLog']).then(
+      ([plantAsset, quantity, harvestLog]) => {
+        expect(true).to.equal(false);
+
+        /*
+        cy.wrap(farmosUtil.getHarvestLog(harvestLog.id)).then((result) => {
+          expect(result.attributes.timestamp).to.contain('1999-01-02');
+          expect(result.attributes.purchase_date).to.contain('1999-01-02');
+
+          expect(result.attributes.name).to.equal('1999-01-02_ts_ARUGULA');
+
+          expect(result.attributes.status).to.equal('done');
+          expect(result.attributes.is_movement).to.equal(true);
+
+          expect(result.relationships.category.length).to.equal(2);
+          expect(result.relationships.category[0].id).to.equal(
+            categoryMap.get('seeding').id
+          );
+          expect(result.relationships.category[1].id).to.equal(
+            categoryMap.get('seeding_tray').id
+          );
+
+          expect(result.relationships.location[0].id).to.equal(
+            greenhouseMap.get('CHUAU').id
+          );
+          expect(result.relationships.location[0].type).to.equal(
+            'asset--structure'
+          );
+          expect(result.relationships.asset[0].id).to.equal(plantAsset.id);
+
+          expect(result.relationships.quantity.length).to.equal(1);
+          expect(result.relationships.quantity[0].id).to.equal(quantity.id);
+          */
+      }
+    );
+  });
+});
