36485955c6ef0f351a3b2fdbe19c187e20285a40
rohanpiya <piyar@dickinson.edu>
2025-10-09 22:03:08 -0400

    Used a method in the computed property such that plants in the table appear in a sorted form date wise (oldest at the top to the newest at the bottom)

--- modules/farm_fd2_school/src/entrypoints/Vue4/App.vue
+++ modules/farm_fd2_school/src/entrypoints/Vue4/App.vue
@@ -48,7 +48,7 @@
           <th>Planted Date</th>
         </tr>
         <tr
-          v-for="plant in plantList"
+          v-for="plant in sortedPlants"
           v-bind:key="plant.id"
         >
           <td>
@@ -133,9 +133,9 @@ export default {
       cropList: ['ARUGULA', 'ASPARAGUS', 'BEAN', 'RADISH'],
       plantList: [
         { id: 1, date: '04/02/2019', location: 'D', bed: '' },
-        { id: 2, date: '04/02/2019', location: 'GHANA', bed: 'GHANA-2' },
-        { id: 3, date: '04/02/2019', location: 'GHANA', bed: 'GHANA-4' },
-        { id: 4, date: '06/05/2019', location: 'GHANA', bed: 'GHANA-4' },
+        { id: 2, date: '04/02/2023', location: 'GHANA', bed: 'GHANA-2' },
+        { id: 3, date: '04/02/2009', location: 'GHANA', bed: 'GHANA-4' },
+        { id: 4, date: '06/05/2015', location: 'GHANA', bed: 'GHANA-4' },
       ],
       unitList: ['BUNCH', 'EACH', 'POUND'],
     };
@@ -148,6 +148,11 @@ export default {
         this.pickedPlant > 0 &&
         this.quantity != '' &&
         this.unit != ''
+      );
+    },
+    sortedPlants() {
+      return [...this.plantList].sort(
+        (a, b) => new Date(a.date) - new Date(b.date)
       );
     },
   },
