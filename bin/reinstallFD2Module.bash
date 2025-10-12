4d62cb93e2ba071a6a8c042edfc1b1ef7f54eaac
braughtg <braught@dickinson.edu>
2025-10-12 09:30:58 -0400

    adds script to reinstall fd2 module

--- bin/reinstallFD2Module.bash
+++ bin/reinstallFD2Module.bash
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-echo "Uninstalling farm_fd2 module..."
-echo "  Deleting custom fd2_fields..."
+echo "Deleting custom fd2_fields..."
 docker exec fd2_farmos_school drush php-eval "\Drupal\field\Entity\FieldStorageConfig::loadByName('taxonomy_term', 'fd2_unit_conversions')->delete();"
 docker exec fd2_farmos_school drush php-eval "\Drupal\field\Entity\FieldStorageConfig::loadByName('taxonomy_term', 'fd2_harvest_unit')->delete();"
-echo "  Deleted."
+echo "Deleted."
+echo "Running cron..."
+docker exec fd2_farmos_school drush cron > /dev/null
+echo "Done."
+
+echo "Uninstalling farm_fd2 module..."
 docker exec fd2_farmos_school drush pmu farm_fd2 -y
 echo "Uninstalled."
 
