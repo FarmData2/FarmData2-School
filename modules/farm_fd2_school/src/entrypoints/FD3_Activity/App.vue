c605f836d09db90f03e2b9481aea1bee7a533e06
braughtg <braught@dickinson.edu>
2025-10-26 04:11:32 -0400

    adds mostly empty DateInput component

--- modules/farm_fd2_school/src/entrypoints/FD3_Activity/App.vue
+++ modules/farm_fd2_school/src/entrypoints/FD3_Activity/App.vue
@@ -5,6 +5,8 @@
   >
     <div id="harvest-header"><h1>Harvest</h1></div>
 
+    <DateInput />
+
     <label
       for="harvest-date"
       class="label-margin"
@@ -130,8 +132,12 @@
 </template>
 
 <script>
+import DateInput from '@comps/DateInput/DateInput.vue';
 import * as farmosUtil from '@libs/farmosUtil/farmosUtil';
 export default {
+  components: {
+    DateInput,
+  },
   data() {
     return {
       date: '2019-06-15',
