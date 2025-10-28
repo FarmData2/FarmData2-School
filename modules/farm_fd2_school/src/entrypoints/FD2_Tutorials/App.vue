c5b3af9db761d526829bd71af90b906a21c5ff4b
gracienl <nguyenl1@dickinson.edu>
2025-10-28 18:56:05 -0400

    set cropList

--- modules/farm_fd2_school/src/entrypoints/FD2_Tutorials/App.vue
+++ modules/farm_fd2_school/src/entrypoints/FD2_Tutorials/App.vue
@@ -129,6 +129,7 @@
 </template>
 
 <script>
+import * as farmosUtil from '@libs/farmosUtil/farmosUtil';
 export default {
   data() {
     return {
@@ -197,11 +198,14 @@ export default {
     },
   },
   async created() {
-    const cropsResponse = await fetch(
-      'http://farmos/api/taxonomy_term/plant_type'
-    );
-    const crops = await cropsResponse.json();
-    this.cropList = crops.data;
+    //  const cropsResponse = await fetch(
+    //    'http://farmos/api/taxonomy_term/plant_type'
+    //  );
+    //  const crops = await cropsResponse.json();
+    //  this.cropList = crops.data;
+    const cropsArray = await farmosUtil.getCrops();
+    console.log(cropsArray);
+    this.cropList = cropsArray;
   },
 };
 </script>
