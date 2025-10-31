adfc0ac239c27179baf6615425dc53794bc35bf1
braughtg <braught@dickinson.edu>
2025-10-31 13:10:48 -0400

    updated cypress config

--- modules/farm_fd2_school/10-FD4-Tutorial-Starter/cypress.config.cjs
+++ modules/farm_fd2_school/10-FD4-Tutorial-Starter/cypress.config.cjs
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  screenshotOnRunFailure: false,
+  video: false,
+  trashAssetsBeforeRuns: true,
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 10000,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    supportFile: './cypress/support/e2e.js',
+    specPattern: './*.e2e.cy.js',
   },
 });
