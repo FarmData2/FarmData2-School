ac94d7683ad3a5dbcc833d70d23acd719a6efc50
braughtg <braught@dickinson.edu>
2025-11-01 10:13:28 -0400

    complete starter code for 10 Tutorials

--- modules/farm_fd2_school/10-FD4-Tutorials-Starter/cypress.config.cjs
+++ modules/farm_fd2_school/10-FD4-Tutorials-Starter/cypress.config.cjs
@@ -5,7 +5,6 @@ module.exports = defineConfig({
   video: false,
   trashAssetsBeforeRuns: true,
   chromeWebSecurity: false,
-  defaultCommandTimeout: 10000,
   e2e: {
     supportFile: './cypress/support/e2e.js',
     specPattern: './*.e2e.cy.js',
