b9ee580ff044ef77bb6a27fe582dd7ff6f9d7e24
rohanpiya <piyar@dickinson.edu>
2025-11-06 17:16:56 -0500

    Copied the date input to the dateinput component

--- components/DateInput2/DateInput2.vue
+++ components/DateInput2/DateInput2.vue
@@ -1,5 +1,15 @@
 <template>
   <p>This wil be our DateInput Component</p>
+  <label
+    for="harvest-date"
+    class="label-margin"
+    >Date:</label
+  >
+  <input
+    type="date"
+    id="harvest-date"
+    v-model="date"
+  />
 </template>
 
 <script></script>
