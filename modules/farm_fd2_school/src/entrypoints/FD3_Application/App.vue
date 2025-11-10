d79df355698b951f73a43be7652d510a46e78cfe
AnnaleeBr <brantan@dickinson.edu>
2025-11-10 11:42:42 -0500

    implements the SubmitRestButtons component

--- modules/farm_fd2_school/src/entrypoints/FD3_Application/App.vue
+++ modules/farm_fd2_school/src/entrypoints/FD3_Application/App.vue
@@ -103,20 +103,12 @@
       There are no {{ crop.attributes.name }} plants available for harvest.
     </div>
     <br />
-    <input
-      type="button"
-      id="harvest-submit"
-      value="Submit"
-      class="label-margin"
-      v-bind:disabled="!formValid"
-      v-on:click="submitForm"
-    />
-    <input
-      type="button"
-      id="harvest-reset"
-      value="Reset"
-      v-on:click="resetForm"
-    />
+    <SubmitResetButtons
+      v-bind:enableSubmit="formValid"
+      v-bind:enableReset="true"
+      v-on:submit="submitForm()"
+      v-on:reset="resetForm()"
+    ></SubmitResetButtons>
 
     <hr />
   </div>
@@ -125,11 +117,13 @@
 <script>
 import DateSelector from '@comps/DateSelector/DateSelector.vue';
 import CommentBox from '@comps/CommentBox/CommentBox.vue';
+import SubmitResetButtons from '@comps/SubmitResetButtons/SubmitResetButtons.vue';
 import * as farmosUtil from '@libs/farmosUtil/farmosUtil';
 export default {
   components: {
     DateSelector,
     CommentBox,
+    SubmitResetButtons,
   },
   data() {
     return {
