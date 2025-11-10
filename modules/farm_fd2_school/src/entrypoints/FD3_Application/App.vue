7b2ba6396dc1fe82cc0534f22819390fd81e34d6
AnnaleeBr <brantan@dickinson.edu>
2025-11-10 11:31:13 -0500

    implemented the CommentBox component

--- modules/farm_fd2_school/src/entrypoints/FD3_Application/App.vue
+++ modules/farm_fd2_school/src/entrypoints/FD3_Application/App.vue
@@ -90,13 +90,11 @@
       <span v-if="this.unitList.length === 1">{{ unit.attributes.name }}</span>
       <hr />
 
-      <textarea
+      <CommentBox
         id="harvest-comment"
-        rows="5"
-        cols="35"
-        placeholder="Enter a comment..."
-        v-model.trim.lazy="comment"
-      />
+        data-cy="comment-box"
+        v-model:comment="comment"
+      ></CommentBox>
     </div>
     <div
       id="harvest-no-plants"
@@ -126,10 +124,12 @@
 
 <script>
 import DateSelector from '@comps/DateSelector/DateSelector.vue';
+import CommentBox from '@comps/CommentBox/CommentBox.vue';
 import * as farmosUtil from '@libs/farmosUtil/farmosUtil';
 export default {
   components: {
     DateSelector,
+    CommentBox,
   },
   data() {
     return {
