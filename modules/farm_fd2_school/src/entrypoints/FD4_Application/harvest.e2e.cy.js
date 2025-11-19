d540be5c2fb7502cbd31368d6b407d0d77d4d2bd
Annalee <brantan@dickinson.edu>
2025-11-18 23:08:12 -0500

    tests crop selection with harvestable plants

--- modules/farm_fd2_school/src/entrypoints/FD4_Application/harvest.e2e.cy.js
+++ modules/farm_fd2_school/src/entrypoints/FD4_Application/harvest.e2e.cy.js
@@ -34,4 +34,34 @@ describe('Tests for the Harvest form', () => {
     cy.get('[data-cy="harvest-units"]').should('not.exist');
     cy.get('[data-cy="harvest-comment"]').should('not.exist');
   });
+
+  it('State Test After Crop Selected With Harvestable Plants', () => {
+    cy.get('[data-cy="harvest-crop"] select').select('RADISH');
+    cy.get('[data-cy="harvest-header"]')
+      .should('be.visible')
+      .should('have.text', 'Harvest');
+    cy.get('[data-cy="harvest-date"]')
+      .should('be.visible')
+      .find('input')
+      .should('have.value', '2019-06-15');
+    cy.get('[data-cy="harvest-crop"]')
+      .should('be.visible')
+      .find('select')
+      .should('have.value', 'RADISH');
+    cy.get('[data-cy="harvest-submit-reset"]')
+      .should('be.visible')
+      .find('button')
+      .first()
+      .should('be.disabled');
+    cy.get('[data-cy="harvest-table"]').should('be.visible');
+    cy.get('[data-cy="harvest-quantity"]')
+      .should('be.visible')
+      .find('input')
+      .should('have.value', 1);
+    cy.get('[data-cy="harvest-units"]').should('be.visible');
+    cy.get('[data-cy="harvest-comment"]')
+      .should('be.visible')
+      .find('textarea')
+      .should('have.value', '');
+  });
 });
