671758901d7460515a1bcdf2da770d32c00cf32d
aaronshin43 <aaronshin.exe@gmail.com>
2025-11-29 23:53:37 -0500

    Add tests for initial state of the Harvest form

--- modules/farm_fd2_school/src/entrypoints/FD4_Application/harvest.e2e.cy.js
+++ modules/farm_fd2_school/src/entrypoints/FD4_Application/harvest.e2e.cy.js
@@ -12,7 +12,25 @@ describe('Tests for the Harvest form', () => {
     cy.saveSessionStorage();
   });
 
-  it('Placeholder test', () => {
-    cy.get('[data-cy="does-not-exist"]');
+  it('Test for initial state of Harvest form', () => {
+    cy.get('[data-cy="harvest-header"]')
+      .should('be.visible')
+      .should('have.text', 'Harvest');
+    cy.get('[data-cy="harvest-date"]')
+      .should('be.visible')
+      .find('[data-cy="date-input"]')
+      .should('have.value', '2019-06-15');
+    cy.get('[data-cy="harvest-crop"]')
+      .should('be.visible')
+      .find('[data-cy="crop-selector"]')
+      .should('have.value', '');
+    cy.get('[data-cy="submit-reset-button"]')
+      .should('be.visible')
+      .find('[data-cy="submit-button"]')
+      .should('be.disabled');
+    cy.get('[data-cy="harvest-table"]').should('not.exist');
+    cy.get('[data-cy="harvest-quantity"]').should('not.exist');
+    cy.get('[data-cy="harvest-units"]').should('not.exist');
+    cy.get('[data-cy="harvest-comment"]').should('not.exist');
   });
 });
