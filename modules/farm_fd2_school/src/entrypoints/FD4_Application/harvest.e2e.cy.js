2ab4cdd6d33f868a400447f217631f0d1e44b78a
truongquand <truongq@dickinson.edu>
2025-12-02 01:12:28 -0500

    Finished hw 10

--- modules/farm_fd2_school/src/entrypoints/FD4_Application/harvest.e2e.cy.js
+++ modules/farm_fd2_school/src/entrypoints/FD4_Application/harvest.e2e.cy.js
@@ -3,8 +3,12 @@ describe('Tests for the Harvest form', () => {
     cy.restoreLocalStorage();
     cy.restoreSessionStorage();
 
+    cy.intercept('GET', '**/api/taxonomy_term/plant_type*').as('getCrops');
+
     cy.login('manager1', 'farmdata2');
     cy.visit('fd2_school/FD4_Application');
+
+    cy.wait('@getCrops');
   });
 
   afterEach(() => {
@@ -12,7 +16,66 @@ describe('Tests for the Harvest form', () => {
     cy.saveSessionStorage();
   });
 
-  it('Placeholder test', () => {
-    cy.get('[data-cy="does-not-exist"]');
+  it('Should verify the initial state of the Harvest form upon loading', () => {
+    cy.get('[data-cy="harvest-header"]')
+      .should('be.visible')
+      .and('contain', 'Harvest');
+    
+    cy.get('[data-cy="date-selector"]')
+      .should('be.visible')
+      .find('input')
+      .should('have.value', '2019-06-15');
+
+    cy.get('[data-cy="crop-selector"]')
+      .should('be.visible')
+      .find('select')
+      .should('have.value', ""); 
+    cy.get('[data-cy="submit"]').should('be.visible').and('be.disabled'); 
+    cy.get('[data-cy="reset"]').should('be.visible').and('not.be.disabled');
+
+    cy.get('[data-cy="plant-table"]').should('not.exist');
+    cy.get('[data-cy="quantity-input"]').should('not.exist');
+    cy.get('[data-cy="units-select"]').should('not.exist');
+    cy.get('[data-cy="comment-box"]').should('not.exist');
+  });
+
+  it('Should correctly update the form when a crop with harvestable plants is selected', () => {    cy.get('[data-cy="crop-selector"]').find('select').select('ARUGULA');
+        
+    // Plant Table check
+    cy.get('[data-cy="plant-table"]')
+      .should('be.visible')
+      .find('tr')
+      .its('length')
+      .should('be.gt', 1); 
+    cy.get('[data-cy="plant-table"]')
+      .find('input[type="radio"]')
+      .should('not.be.checked'); 
+    cy.get('[data-cy="quantity-input"]')
+      .should('be.visible')
+      .find('input')
+      .should('have.attr', 'type', 'number')
+      .should('have.value', '1');
+      
+    cy.get('[data-cy="units-select"], span').should('be.visible');
+    cy.get('[data-cy="comment-box"]').should('be.visible');
+    
+    cy.get('[data-cy="submit"]').should('be.disabled');
+    
+    cy.get('#harvest-no-plants').should('not.exist');
+  });
+
+  it('Should correctly display a message when a selected crop has no harvestable plants', () => {
+    cy.get('[data-cy="crop-selector"]').find('select').select('BEANS'); 
+
+    cy.get('#harvest-no-plants')
+      .should('be.visible')
+      .and('contain', 'There are no BEANS plants available for harvest.');
+
+    cy.get('[data-cy="plant-table"]').should('not.exist');
+    cy.get('[data-cy="quantity-input"]').should('not.exist');
+    cy.get('[data-cy="units-select"]').should('not.exist');
+    cy.get('[data-cy="comment-box"]').should('not.exist');
+
+    cy.get('[data-cy="submit"]').should('be.disabled');
   });
 });
